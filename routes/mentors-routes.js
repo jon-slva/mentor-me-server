4b3844873a9740bff8f320063f4113b65a3fa3f0
Jonathan Silva <jonathan.silva167@gmail.com>
2023-11-27 15:55:30 -0500

    I swear this is the last one

--- routes/mentors-routes.js
+++ routes/mentors-routes.js
@@ -6,8 +6,8 @@ const mentorsController = require("../controllers/mentors-controller");
 router.route("/")
     .get(mentorsController.search);
 
-router.route("/mentor/:mentorId")
-    .get(mentorsController.get);
+// router.route("/mentor/:mentorId")
+// .get(mentorsController.get);
 
 
 
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const userController = require("../controllers/user-controller");
 
 
-
 router.route("/login")
     .post(userController.loginFunc)
     .get(userController.authorizeUser)
