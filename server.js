4a82fb5444b1f32edb2256cd27a72276bad3f0da
Jonathan Silva <jonathan.silva167@gmail.com>
2025-05-26 01:58:01 -0400

    - update user profile images and portfolio links in seed data - Reorganize and label server.js

--- server.js
+++ server.js
@@ -1,35 +1,32 @@
+// Module Imports
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+
+// Configuration
 const app = express();
-require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+console.log(PORT);
+
+// Route Imports
 const mentorsRoutes = require('./routes/mentors-routes');
 const menteesRoutes = require('./routes/mentees-routes');
 const userRoutes = require('./routes/user-routes');
 
-const PORT = process.env.PORT || 3000;
-
-console.log(PORT)
-
 // Middleware
 app.use(cors());
 app.use(express.json());
-// Serve static files from the "public" directory
 app.use(express.static('public'));
 
-
-// Sample route
+// Routes
 app.get('/', (req, res) => {
     res.json({ message: 'Hello, world!' });
 });
-
 app.use('/api/mentors', mentorsRoutes);
-
 app.use("/api/mentees", menteesRoutes);
-
 app.use("/api/users", userRoutes);
 
-
-// Start the server
+// Server Initialization
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
